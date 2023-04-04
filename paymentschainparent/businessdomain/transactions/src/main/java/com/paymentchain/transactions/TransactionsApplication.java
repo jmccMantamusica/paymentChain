530b9eaa3fbda89451433ddb57bd75943996626b
jcagigas <jose.maria.cagigas@gmail.com>
2023-04-04 17:26:59 +0200

    tratamientos de excepciones

--- paymentschainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/TransactionsApplication.java
+++ paymentschainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/TransactionsApplication.java
@@ -1,13 +1,16 @@
 package com.paymentchain.transactions;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableEurekaClient
 public class TransactionsApplication {
 
@@ -16,11 +19,9 @@ public class TransactionsApplication {
 	}
         
          @Bean
-  public GroupedOpenApi publicApi() {
-      return GroupedOpenApi.builder()
-              .group("springshop-public")            
-              .packagesToScan("com.paymentchain")
-              .build();
-  }  
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.paymentchain")).build();
+    }
 
 }
