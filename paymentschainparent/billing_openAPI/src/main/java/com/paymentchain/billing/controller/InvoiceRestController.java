b0bb30aa9ccbedfa67cba20da78f833e4e5810b4
jcagigas <jose.maria.cagigas@gmail.com>
2023-04-05 13:02:49 +0200

    mapstruck dto entity dto

--- paymentschainparent/billing_openAPI/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
+++ paymentschainparent/billing_openAPI/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
@@ -6,6 +6,10 @@
 package com.paymentchain.billing.controller;
 
 
+import com.paymentchain.billing.common.InvoiceRequestMapper;
+import com.paymentchain.billing.common.InvoiceResponseMapper;
+import com.paymentchain.billing.dto.InvoiceRequest;
+import com.paymentchain.billing.dto.InvoiceResponse;
 import com.paymentchain.billing.entities.Invoice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,35 +43,51 @@ public class InvoiceRestController {
     @Autowired
     InvoiceRepository billingRepository;    
     
+      @Autowired
+    InvoiceRequestMapper irm;
+    
+    @Autowired
+    InvoiceResponseMapper irspm;
+    
     @Operation(description = "Return all invoices bundled into Response", summary ="Return 204 if no data found")
     @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "Exito"),
     @ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping()
-    public List<Invoice> list() {
-       return  billingRepository.findAll();
+    public List<InvoiceResponse> list() {
+        List<Invoice> findAll = billingRepository.findAll();
+        List<InvoiceResponse> InvoiceListToInvoiceResposeList = irspm.InvoiceListToInvoiceResposeList(findAll);
+        return InvoiceListToInvoiceResposeList;
     } 
     
     @GetMapping("/{id}")
-    public Invoice get(@PathVariable String id) {
+    public InvoiceResponse get(@PathVariable String id) {
         Optional<Invoice> findById = billingRepository.findById(Long.valueOf(id));
-        return findById.get();
+        InvoiceResponse InvoiceToInvoiceRespose = irspm.InvoiceToInvoiceRespose(findById.get());
+        return InvoiceToInvoiceRespose;
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Invoice input) {
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody InvoiceRequest input) {
        Invoice save = null; 
         Optional<Invoice> findById = billingRepository.findById(Long.valueOf(id));
         Invoice get = findById.get();
-        if(get != null){             
-                    save = billingRepository.save(get);  
+        if(get != null){   
+               get.setAmount(input.getAmount());
+               get.setDetail(input.getDetail());
+               get.setCustomerId(input.getCustomer());
+               get.setNumber(input.getNumber());
+                    save = billingRepository.save(get);                      
         }
-        return ResponseEntity.ok(save);
+          InvoiceResponse InvoiceToInvoiceRespose = irspm.InvoiceToInvoiceRespose(save);
+        return ResponseEntity.ok(InvoiceToInvoiceRespose);
     }
     
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody Invoice input) { 
-        Invoice save = billingRepository.save(input);     
-        return ResponseEntity.ok(save);
+    public ResponseEntity<?> post(@RequestBody InvoiceRequest input) { 
+         Invoice InvoiceRequestToInvoice = irm.InvoiceRequestToInvoice(input);  
+        Invoice save = billingRepository.save(InvoiceRequestToInvoice);    
+        InvoiceResponse InvoiceToInvoiceRespose = irspm.InvoiceToInvoiceRespose(save);
+        return ResponseEntity.ok(InvoiceToInvoiceRespose);
     }
     
     @DeleteMapping("/{id}")
--- paymentschainparent/billing_openAPI/src/main/java/com/paymentchain/billing/entities/Invoice.java
+++ paymentschainparent/billing_openAPI/src/main/java/com/paymentchain/billing/entities/Invoice.java
@@ -20,14 +20,13 @@ import lombok.Data;
  */
 @Entity
 @Data
-@Schema(name = "Invoice", description = "Model represent a invoice on database")
 public class Invoice {
    @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
    private long id;
-     @Schema(name = "customerId", required = true,example = "2", defaultValue = "1", description = "Unique Id of customer that represent the owner of invoice")
+   
    private long customerId;
-       @Schema(name = "number", required = true,example = "3", defaultValue = "8", description = "Number given on fisical invoice")
+
    private String number;
    private String detail;
    private double amount;  
