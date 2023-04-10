1388db29d6723de7d66232754c14e00c89054a98
jcagigas <jose.maria.cagigas@gmail.com>
2023-04-10 11:45:24 +0200

    version final

--- paymentschainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/TransactionsApplication.java
+++ paymentschainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/TransactionsApplication.java
@@ -9,6 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
@@ -23,5 +24,4 @@ public class TransactionsApplication {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.paymentchain")).build();
     }
-
 }
